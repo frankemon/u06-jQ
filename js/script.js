72897af00e5049617da9784a879f91fadba91a11
frankemon <frankie.hugo@hotmail.com>
2021-02-27 15:55:46 +0100

    fix and simplify sortable function

--- js/script.js
+++ js/script.js
@@ -1,16 +1,6 @@
-// $( function() {
-//     $( "#sortable1, #sortable2, #sortable3" ).sortable({
-//       connectWith: ".connectedSortable"
-//     }).disableSelection();
-//   } );
+$( function() {
+    $( ".card-column" ).sortable({
+      connectWith: ".card"
+    }).disableSelection();
+  } );
 
-$(function () {
-    $(".card-column").sortable({
-        connectWith: ".card-column",
-        items: '.card',
-        // placeholder: 'ui-state-highlight',
-        // forcePlaceholderSize: true,
-        // distance: 0.1,
-        // dropOnEmpty: true
-    });
-})
