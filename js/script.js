e75a192c91705125e81e5cae9482ac52e346f629
frankemon <frankie.hugo@hotmail.com>
2021-02-27 16:56:40 +0100

    add basic dialog to single task, add button styling

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
+// Sortable
 $( function() {
     $( ".card-column" ).sortable({
       connectWith: ".card"
     }).disableSelection();
   } );
 
+  // Dialog
+  $( function() {
+    $( "#dialog" ).dialog({
+      autoOpen: false,
+      show: {
+        effect: "fade",
+        duration: 200
+      },
+      hide: {
+        effect: "fade",
+        duration: 300
+      }
+    });
+ 
+    $( "#opener" ).on( "click", function() {
+      $( "#dialog" ).dialog( "open" );
+    });
+  } );
