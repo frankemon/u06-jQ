434d514204853e454eafa108559983f58e845437
frankemon <frankie.hugo@hotmail.com>
2021-03-07 14:48:10 +0100

    add tabs fx

--- js/script.js
+++ js/script.js
@@ -20,34 +20,20 @@ $( function() {
       }
     });
  
-    $( ".opener" ).on( "click", function() {
-      $( ".dialog" ).dialog( "open" );
+    $( "#opener" ).on( "click", function() {
+      $( "#dialog" ).dialog( "open" );
     });
   } );
 
   $('.dialog').dialog({
     height: "auto",
     width: 300,
-    modal: false,
-    resizable: true,
-    dialogClass: 'no-close success-dialog'
+    // modal: false,
+    // resizable: true,
+    // dialogClass: 'no-close success-dialog'
 });
 
-// Dialog w/ delete
-// $( function() {
-//   $( "#dialog-confirm" ).dialog({
-//     autoOpen: false,
-//     resizable: false,
-//     height: "auto",
-//     width: 300,
-//     modal: true,
-//     buttons: {
-//       "Delete all items": function() {
-//         $( this ).dialog( "close" );
-//       },
-//       Cancel: function() {
-//         $( this ).dialog( "close" );
-//       }
-//     }
-//   });
-// } );
+// Tabs
+$( function() {
+  $( "#tabs" ).tabs();
+} );
