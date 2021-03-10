0b4fa61f543dafa01bfa63dc2ae699d8946234f7
frankemon <frankie.hugo@hotmail.com>
2021-03-10 18:55:12 +0100

    fix custom widget

--- js/script.js
+++ js/script.js
@@ -1,70 +1,52 @@
 $(() => {
-  console.log('JQuery ready');
+  console.log("JQuery ready");
 
   // Sortable lists
-    $( ".connectedSortable" ).sortable({
-      connectWith: ".connectedSortable"
-    }).disableSelection();
-  
-  // Dialog 
-    $( "#dialog" ).dialog({
-      autoOpen: false,
-      show: {
-        effect: "fade",
-        duration: 200
-      },
-      hide: {
-        effect: "fade",
-        duration: 300
-      }
-    });
-    
-    $( "#opener" ).on( "click", function() {
-      $( "#dialog" ).dialog( "open" );
-    });
-  
-  $('.dialog').dialog({
-    height: "auto",
-    width: "100"
+  $(".connectedSortable")
+    .sortable({
+      connectWith: ".connectedSortable",
+    })
+    .disableSelection();
+
+  // Dialog
+  $("#dialog").dialog({
+    autoOpen: false,
+    show: {
+      effect: "fade",
+      duration: 200,
+    },
+    hide: {
+      effect: "fade",
+      duration: 300,
+    },
   });
-  
+
+  $("#opener").on("click", function () {
+    $("#dialog").dialog("open");
+  });
+
+  $(".dialog").dialog({
+    height: "auto",
+    width: "100",
+  });
+
   // Tabs
-    $( "#tabs" ).tabs();
-  
+  $("#tabs").tabs();
+
   // Datepicker
-    $( "#datepicker1, #datepicker2, #datepicker3" ).datepicker();
-  
+  $("#datepicker1, #datepicker2, #datepicker3").datepicker();
 
   // My custom widget
   // this widget adds a line through text to signify that it is complete
-  
-//  const taskList = $('#taskList');  // Save html ul element as const
-//  let state = false;
-  
-  $.widget( "custom.toggleList", {
-    options: {
-      value: 0  // maybe use this to set line-through
-    },
 
-        _create: function() {
-          console.log("hello");
-          // $( "#taskList li" ).on( "click", function() {
-          //  $(this).toggleClass('line-through')
-          //   });
-
-        },
-
-     
+  $.widget("custom.toggleList", {
+    _create: function () {
+      this.element.addClass("toggle-list")
+      $("li", this.element).on("click", function () {
+        $(this).toggleClass("line-through");
       });
-      
-      
-      
-      console.log("bye");
-      
-    });// ----------------- close jq ----------------
-    
-      $('#taskList').toggleList();
+    },
+  });
 
-
-
-
+  $("#taskList").toggleList();
+}); // ----------------- close jq ----------------
